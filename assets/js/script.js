34abfd47fdddb52599492e8d96f75d6bb23bc34a
TaeJeong <i@taeyjeong.com>
2021-02-16 14:14:17 -0800

    setting password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+// setting value for list of characters
+var charList = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_+={}[]|?\/<>,.:;'";
+// creating how the generatePassword function runs
+function generatePassword () {
+  var pass = prompt("Number of characters for password: 8-128"); // user's choice of password length
+  if (pass >= 8 && pass <= 128) { // password must be 8-128 characters
+    var charChoice = "" // user's character choices from charList
+    var finalWord = ""; // random variation of user's choices (charChoice) from charList
+    console.log(pass)
+  } else {
+    alert("Please try again."); // if the password doesnt fit 8-128 characters, it resets screen
+    return;
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
